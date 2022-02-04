1ac6ac13ecbaee52e896568c7e12ac72849a94a9
Jack Hu <zhongth@g.clemson.edu>
2022-02-03 21:01:52 -0500

    Fix formatting

--- src/p1.py
+++ src/p1.py
@@ -62,6 +62,34 @@ def radius_mass_function(mass):
     return (a[0]*mass**2.5 + a[1]*mass**6.5 + a[2]*mass**11 + a[3]*mass**19 + a[4]*mass**19.5) / (a[5] + a[6]*mass**2 + a[7]*mass**8.5 + a[8]*mass**18.5 + a[9]*mass**19.5)
 
 
+def simpsons_rule_integrate(integrand, low=0, high=1, step=10):
+    """
+    Simpson's rule 1-D numerical integration method. It takes the target integrand, 
+    which must be an integratable function defined elsewhere that returns a 
+    numerical value, with integration bounds defined by low and high, and
+    it integrates n = step - 1 times and produces an approximate result.
+
+    integrand: function definition that represents an integratable,
+               1 dimensional function.
+    low:       lower bound of integration. Default is 0.
+    high:      upper bound of integration. Default is 1.
+    step:      number of steps for integration to take place. Default is 10 steps.
+    TODO:      Add time-keeping feature to this method.
+    """
+
+    steps = np.linspace(low, high, num=step)
+    result = 0.0
+    j = 0
+
+    # Scheme follows defintion in Scientific Computing textbook by Heath.
+    for i in steps[1:]:
+        a = steps[j]
+        result += ((i - a) / 6) * (integrand(a) + 4 *
+                                   integrand((a + i) / 2) + integrand(i))
+        j += 1
+
+    return result
+
 # The following functions come from the Inversion_Sampling.pdf document,
 # provided by Ben Amend.
 
@@ -90,35 +118,6 @@ def probability_density_function(mass, imf, max_mass, min_mass=0):
     A = 1 / (simpsons_rule_integrate(imf, min_mass, max_mass, 100))
 
     return A * imf(mass)
-
-
-def simpsons_rule_integrate(integrand, low=0, high=1, step=10):
-    """
-    Simpson's rule 1-D numerical integration method. It takes the target integrand, 
-    which must be an integratable function defined elsewhere that returns a 
-    numerical value, with integration bounds defined by low and high, and
-    it integrates n = step - 1 times and produces an approximate result.
-
-    integrand: function definition that represents an integratable,
-               1 dimensional function.
-    low:       lower bound of integration. Default is 0.
-    high:      upper bound of integration. Default is 1.
-    step:      number of steps for integration to take place. Default is 10 steps.
-    TODO:      Add time-keeping feature to this method.
-    """
-
-    steps = np.linspace(low, high, num=step)
-    result = 0.0
-    j = 0
-
-    # Scheme follows defintion in Scientific Computing textbook by Heath.
-    for i in steps[1:]:
-        a = steps[j]
-        result += ((i - a) / 6) * (integrand(a) + 4 *
-                                   integrand((a + i) / 2) + integrand(i))
-        j += 1
-
-    return result
 
 
 def test_function(x, alp=1):
